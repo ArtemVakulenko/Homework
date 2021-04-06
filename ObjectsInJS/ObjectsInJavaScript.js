9d5ffb1a51605df165e6d2a1aa8266bc46ed52bf
ArtemVakulenko <80762228+ArtemVakulenko@users.noreply.github.com>
2021-04-06 22:14:31 +0300

    made progress in ObjectsInJs

--- ObjectsInJS/ObjectsInJavaScript.js
+++ ObjectsInJS/ObjectsInJavaScript.js
@@ -29,9 +29,42 @@ function findOddOrEven(array){
 
 // 3. Принимая массив объектов и случайную строку. У объектов может быть ключ: «title» с разными значениями. Создайте алгоритм, который фильтрует массив, заданный как первый параметр, и возвращает массив объектов, которые содержат в своих заголовках заданную строку в качестве второго параметра (без учета регистра).
 
-
-
+var arrayObjects = [
+    {name: 'obj1', title: 'js'},
+    {name: 'obj2', title: 'JS'},
+    {name: 'obj3', title: 'JavaScript'},
+    {name: 'obj4', title: 'i love JS'},
+    {name: 'obj5', title: 'jsIsTheBest'},
+    {name: 'obj6', title: 'PythonIsTheBest'},
+    {name: 'obj7', notATitle: 'JS'},
+]
+function findTitle (array, string){
+    var filteredArray = []
+    string = string.toLowerCase()
+    for(var i = 0; i < array.length; i++){
+        if(array[i].hasOwnProperty('title')){
+            array[i]['title'].toLowerCase()
+            if(array[i]['title'].includes(string)){
+                filteredArray.unshift(array[i])
+            }
+        }
+    }
+    return filteredArray
+}
+// console.log(findTitle(arrayObjects, 'js'));
 //  4. Принимая строку, ваша функция должна вернуть обьект, в котором ключи – символы строки, значение – количество повторений символов в строке
+
+function breakDownString(str){
+    var strObj = {}
+    for (var i = 0; i < str.length; i++){
+        strObj[str[i]] = 0
+    }
+    for (var i = 0; i < str.length; i++){
+        strObj[str[i]]++
+    }
+    return strObj
+}
+// console.log(breakDownString('qwweeerrrrtttttyaa'));
 
 // 5. Принимая число, ваша функция должна найти следующий положительный палиндром большего размера.
 
@@ -56,5 +89,5 @@ function isPalindrome(str) {
         return false
     }
   }
-console.log(isPalindrome('151'));
-console.log(getNextPalindrome(146));
+// console.log(isPalindrome('151'));
+// console.log(getNextPalindrome(146));
